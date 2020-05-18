99a35b140ee767411dd95d2f134a07018017f92b
Sidão <sidaoswat@hotmail.com>
2020-05-18 17:58:30 -0300

    adicionando e refatorando componente de menu

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -2,15 +2,16 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
 import { IonicModule } from '@ionic/angular';
+import { MenuComponent } from './menu/menu.component';
 
 
 
 @NgModule({
-  declarations: [HeaderComponent],
+  declarations: [HeaderComponent, MenuComponent],
   imports: [
     CommonModule,
     IonicModule
   ],
-  exports: [HeaderComponent]
+  exports: [HeaderComponent, MenuComponent]
 })
 export class ComponentsModule { }
