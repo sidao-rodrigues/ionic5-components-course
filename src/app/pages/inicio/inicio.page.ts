42b09511ada50ebe2955a383214e10aeda20fdf2
Sidão <sidaoswat@hotmail.com>
2020-05-18 17:11:30 -0300

    criando página de datas

--- src/app/pages/inicio/inicio.page.ts
+++ src/app/pages/inicio/inicio.page.ts
@@ -23,6 +23,11 @@ export class InicioPage implements OnInit {
       nome: 'Cards',
       link: '/cards'
     },
+    {
+      icone: 'calendar-outline',
+      nome: 'Datas',
+      link: '/datas'
+    },
   ];
 
   constructor() { }
